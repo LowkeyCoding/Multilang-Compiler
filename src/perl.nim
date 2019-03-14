45ffd79c44b9174daee1eb7de2b6275683c5b559
Kaette <kaette@github.com>
2019-03-14 10:36:49 +0100

    Perl is gay

--- src/perl.nim
+++ src/perl.nim
@@ -1,3 +1,15 @@
-#Start of 
+import ./libs/strutil/src/strutil.nim
+import os
+
+#Start of lexical analysis
 echo "Initiating compiler"
+echo "Starting lexical analysis"
+
+const
+    tksymbols
+    	'%': tkMod,
+    	'+': tkAdd,
+    	'-': tkSub,
+    	'/': tkDiv,
+    	'*': tkMul,
 
