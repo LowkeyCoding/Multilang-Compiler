b12475231c190b35ce614d92d6c4b55fdc9f9b54
Kaette <kaette@github.com>
2019-03-28 10:07:16 +0100

    fs

--- src/perl.nim
+++ src/perl.nim
@@ -1,3 +1,6 @@
+#Dette er en lexical analyzer til sproget perl.
+
+#These are the libraries and modules we have imported.
 import ./libs/strutil/src/strutil.nim
 import os
 
@@ -6,10 +9,39 @@ echo "Initiating compiler"
 echo "Starting lexical analysis"
 
 const
-    tksymbols
-    	'%': tkMod,
-    	'+': tkAdd,
-    	'-': tkSub,
-    	'/': tkDiv,
-    	'*': tkMul,
+    tksymbols = {
+    	'%': modi,
+    	'+': add,
+    	'-': subt,
+    	'/': divi,
+    	'*': mult,
+	';': semi,
+	',': comm,
+	'{': rbra,
+	'}': lbra,
+	'(': rpar,
+	')': lpar,
+	}
+symbols = getSymbols(tksymbols)
 
+proc 
+
+type
+    tKind = enum
+    	unow = "TOKEN_UNKNOWN",
+    	mult = "multiply",
+    	divi = "divide",
+    	modi = "modify",
+    	subt = "subtract",
+    	semi = "semicolon",
+    	comm = "comma",
+    	rbra = "rightBracket",
+    	lbra = "leftBracket",
+    	rpar = "rightParanthesis",
+    	lpar = "leftParanthesis",
+
+
+
+token = object
+	kind: tokenKind
+	value: string
