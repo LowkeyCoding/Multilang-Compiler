ccdc28ef51d4fbb8c06a0d2e587a0a6574bd29d5
Kaette <kaette.frydensbjerg@gmail.com>
2019-04-02 08:53:26 +0200

    perl

--- src/perl.nim
+++ src/perl.nim
@@ -75,3 +75,41 @@ proc conToken(text: var string; tkl: var init): token =
     	(tkind, tkl) = ()
 
     elif text[0] in symbols: (tkind, tkl) = (
+
+proc tokenize*(text: string): seq[tokenAnn]=
+	result = newSeq[tokenAnn]()
+	var
+		lineNo, colNo: int = 1
+		text=text
+		token: Token
+		tokenLength: int
+
+	while text.len > 0:
+		strip(text, lineNo, colNo)
+		token=consumeToken(text, tokenLength)
+		result.add TokenAnn(token: token, line: lineNo, column: colNo)
+		inc colNo, tokenLength
+
+proc output*(s: seq[TokenAnn]): string=
+	var
+		tokenKind: tokenKind
+		value: string
+		line, column: int
+
+	for tokenAnn in items(s):
+		line=tokenAnn.line
+		column=tokenAnn.column
+		tokenKind=tokenAnn.token.kind
+		value=tokenAnn.token.value
+		result.add(
+			fmt"{line:>5}{column:>7}{tokenKind:<15}{value}"
+				.strip(leading=false) & "\n")
+	)
+
+when isMainModule:
+	import os
+
+	let input=if paramCount() > 0: readFile paramStr(1)
+else: readAll stdin
+
+echo input.tokenize.output
