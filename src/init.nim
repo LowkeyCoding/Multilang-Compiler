6228ab76c7c8d842a784cc50d64f23d4f87fd038
Kaette <kaette.frydensbjerg@gmail.com>
2019-04-02 11:17:02 +0200

    Switched init files

--- src/init.nim
+++ src/init.nim
@@ -1,3 +1,16 @@
+#This is the init file. This is what on windows would be an .exe file.
+#This file the one you interact with when you input a file for compilation.
+
 import os
 
-echo
+
+
+if #-help or incorrect input
+    echo """
+        Usage:
+        --BF: Handle input file as BrainFuck
+        --Perl: Handle the input file as Perl
+        
+        -V: Give verbose output
+        -
+        """
